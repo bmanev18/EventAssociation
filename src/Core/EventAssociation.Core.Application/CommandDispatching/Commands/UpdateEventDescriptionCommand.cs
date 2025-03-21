9f6f8958710c8733167b769eec208609885ee644
Bozhidar Manev <95079544+bmanev18@users.noreply.github.com>
2025-03-21 22:56:56 +0000

    UpdateEventDescriptionHandler

---
+++ src/Core/EventAssociation.Core.Application/CommandDispatching/Commands/UpdateEventDescriptionCommand.cs
@@ -0,0 +1,26 @@
+﻿using EventAssociation.Core.Domain.Aggregates.Event;
+using EventAssociation.Core.Domain.Aggregates.Event.Values;
+using EventAssociation.Core.Domain.Common;
+using EventAssociation.Core.Tools.OperationResult;
+
+namespace EventAssociation.Core.Application.CommandDispatching.Commands;
+
+public class UpdateEventDescriptionCommand
+{
+    internal EventId Id { get; }
+    internal EventDescription Description { get; }
+
+    private UpdateEventDescriptionCommand(EventDescription description)
+    {
+        Description = description;
+    }
+
+    public static Result<UpdateEventDescriptionCommand> Create(string id, string description)
+    {
+        // new EventId();
+        var result = EventDescription.CreateEventDescription(description);
+        return !result.IsSuccess
+            ? Result<UpdateEventDescriptionCommand>.Err(result.UnwrapErr().ToArray())
+            : Result<UpdateEventDescriptionCommand>.Ok(new UpdateEventDescriptionCommand(result.Unwrap()));
+    }
+}
---
+++ src/Core/EventAssociation.Core.Application/CommandDispatching/Features/UpdateEventDescriptionHandler.cs
@@ -0,0 +1,36 @@
+﻿using EventAssociation.Core.Application.CommandDispatching.Commands;
+using EventAssociation.Core.Domain.Aggregates.Event;
+using EventAssociation.Core.Domain.Common;
+using EventAssociation.Core.Tools.OperationResult;
+
+namespace EventAssociation.Core.Application.CommandDispatching.Features;
+
+public class UpdateEventDescriptionHandler
+{
+    private readonly IEventRepository repository;
+    private readonly IUnitOfWork uow;
+
+    
+    internal UpdateEventDescriptionHandler(IEventRepository repository, IUnitOfWork uow) =>
+        (this.repository, this.uow) = (repository, uow);
+    
+    public async Task<Result<None>> HandleAsync(UpdateEventDescriptionCommand command)
+    {
+        var getEvent = await repository.GetAsync(command.Id);
+
+        if (!getEvent.IsSuccess)
+        {
+            return Result<None>.Err(getEvent.UnwrapErr().ToArray());
+        }
+
+        var changeResult = getEvent.Unwrap().ChangeDescription(command.Description);
+
+        if (!changeResult.IsSuccess)
+        {
+            return Result<None>.Err(changeResult.UnwrapErr().ToArray());
+        }
+
+        await uow.SaveChangesAsync();
+        return Result<None>.Ok(None.Value);
+    }
+}
