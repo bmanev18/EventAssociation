53c6660029d0319654be08c772beba6a09e3f1eb
Bozhidar Manev <95079544+bmanev18@users.noreply.github.com>
2025-03-22 00:20:19 +0000

    UpdateEventTimesHandler

---
+++ src/Core/EventAssociation.Core.Application/CommandDispatching/Commands/UpdateEventTimesCommand.cs
@@ -0,0 +1,34 @@
+﻿using EventAssociation.Core.Domain.Aggregates.Event.Values;
+using EventAssociation.Core.Tools.OperationResult;
+
+namespace EventAssociation.Core.Application.CommandDispatching.Commands;
+
+public class UpdateEventTimesCommand
+{
+    internal EventId Id { get; }
+    internal EventTime? StartDate { get; }
+    internal EventTime? EndDate { get; }
+
+    private UpdateEventTimesCommand(EventTime? startDate, EventTime? endDate)
+    {
+        StartDate = startDate;
+        EndDate = endDate;
+    }
+
+    public static Result<UpdateEventTimesCommand> Create(string id, string startDate, string endDate)
+    {
+        var creatingStartDate = EventTime.Create(startDate);
+        var creatingEndDate = EventTime.Create(endDate);
+
+        var results = new List<Result<EventTime>>() { creatingStartDate, creatingEndDate };
+        var assertion = Result<EventTime>.AssertResponses(results);
+
+        if (!assertion.IsSuccess)
+        {
+            return Result<UpdateEventTimesCommand>.Err(assertion.UnwrapErr().ToArray());
+        }
+
+        return Result<UpdateEventTimesCommand>.Ok(new UpdateEventTimesCommand(creatingStartDate.Unwrap(),
+            creatingEndDate.Unwrap()));
+    }
+}
---
+++ src/Core/EventAssociation.Core.Application/CommandDispatching/Features/UpdateEventTimesHandler.cs
@@ -0,0 +1,37 @@
+﻿using EventAssociation.Core.Application.CommandDispatching.Commands;
+using EventAssociation.Core.Domain.Aggregates.Event;
+using EventAssociation.Core.Domain.Common;
+using EventAssociation.Core.Tools.OperationResult;
+
+namespace EventAssociation.Core.Application.CommandDispatching.Features;
+
+public class UpdateEventTimesHandler : ICommandHandler<UpdateEventTimesCommand>
+{
+    private readonly IEventRepository repository;
+    private readonly IUnitOfWork uow;
+
+    internal UpdateEventTimesHandler(IEventRepository repository, IUnitOfWork uow) =>
+        (this.repository, this.uow) = (repository, uow);
+
+
+    public async Task<Result<None>> HandleAsync(UpdateEventTimesCommand command)
+    {
+        var getEvent = await repository.GetAsync(command.Id);
+
+        if (!getEvent.IsSuccess)
+        {
+            return Result<None>.Err(getEvent.UnwrapErr().ToArray());
+        }
+
+        // TODO 
+        var changedTimes = getEvent.Unwrap().ChangeTimes(command.StartDate, command.EndDate);
+
+        if (!changedTimes.IsSuccess)
+        {
+            return Result<None>.Err(changedTimes.UnwrapErr().ToArray());
+        }
+
+        await uow.SaveChangesAsync();
+        return Result<None>.Ok(None.Value);
+    }
+}
