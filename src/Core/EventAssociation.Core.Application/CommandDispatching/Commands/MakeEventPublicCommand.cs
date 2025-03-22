e81383ca5fbaacad10a6769f7f32f65219e4f5ba
Bozhidar Manev <95079544+bmanev18@users.noreply.github.com>
2025-03-22 00:43:34 +0000

    MakeEventPublicHandler

---
+++ src/Core/EventAssociation.Core.Application/CommandDispatching/Commands/MakeEventPublicCommand.cs
@@ -0,0 +1,22 @@
+﻿using EventAssociation.Core.Domain.Aggregates.Event.Values;
+using EventAssociation.Core.Tools.OperationResult;
+
+namespace EventAssociation.Core.Application.CommandDispatching.Commands;
+
+public class MakeEventPublicCommand
+{
+    internal EventId Id { get; }
+
+    private MakeEventPublicCommand(EventId id)
+    {
+        Id = id;
+    }
+
+    public static Result<MakeEventPublicCommand> Create(string id)
+    {
+        var validId = EventId.Create(id);
+        return validId.IsSuccess
+            ? Result<MakeEventPublicCommand>.Ok(new MakeEventPublicCommand(validId.Unwrap()))
+            : Result<MakeEventPublicCommand>.Err(validId.UnwrapErr().ToArray());
+    }
+}
---
+++ src/Core/EventAssociation.Core.Application/CommandDispatching/Features/MakeEventPublicHandler.cs
@@ -0,0 +1,31 @@
+﻿using EventAssociation.Core.Application.CommandDispatching.Commands;
+using EventAssociation.Core.Domain.Aggregates.Event;
+using EventAssociation.Core.Domain.Common;
+using EventAssociation.Core.Tools.OperationResult;
+
+namespace EventAssociation.Core.Application.CommandDispatching.Features;
+
+public class MakeEventPublicHandler : ICommandHandler<MakeEventPublicCommand>
+{
+    private readonly IEventRepository repository;
+    private readonly IUnitOfWork uow;
+
+    public async Task<Result<None>> HandleAsync(MakeEventPublicCommand command)
+    {
+        var getEvent = await repository.GetAsync(command.Id);
+        if (!getEvent.IsSuccess)
+        {
+            return Result<None>.Err(getEvent.UnwrapErr().ToArray());
+        }
+
+        var changeEventTypeToPublic = getEvent.Unwrap().ChangeEventTypeToPublic();
+
+        if (changeEventTypeToPublic.IsSuccess)
+        {
+            return changeEventTypeToPublic;
+        }
+
+        await uow.SaveChangesAsync();
+        return Result<None>.Err(changeEventTypeToPublic.UnwrapErr().ToArray());
+    }
+}
