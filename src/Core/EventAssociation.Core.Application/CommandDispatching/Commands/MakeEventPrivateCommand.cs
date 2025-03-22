537d0660d1ac7c2758dcb21d1c27256d69d7071f
Bozhidar Manev <95079544+bmanev18@users.noreply.github.com>
2025-03-22 00:43:49 +0000

    MakeEventPrivateHandler

---
+++ src/Core/EventAssociation.Core.Application/CommandDispatching/Commands/MakeEventPrivateCommand.cs
@@ -0,0 +1,23 @@
+﻿using EventAssociation.Core.Application.CommandDispatching.Commands;
+using EventAssociation.Core.Domain.Aggregates.Event.Values;
+using EventAssociation.Core.Tools.OperationResult;
+
+namespace EventAssociation.Core.Application.CommandDispatching.Features;
+
+public class MakeEventPrivateCommand
+{
+    internal EventId Id { get; }
+
+    private MakeEventPrivateCommand(EventId id)
+    {
+        Id = id;
+    }
+
+    public static Result<MakeEventPrivateCommand> Create(string id)
+    {
+        var validId = EventId.Create(id);
+        return validId.IsSuccess
+            ? Result<MakeEventPrivateCommand>.Ok(new MakeEventPrivateCommand(validId.Unwrap()))
+            : Result<MakeEventPrivateCommand>.Err(validId.UnwrapErr().ToArray());
+    }
+}
---
+++ src/Core/EventAssociation.Core.Application/CommandDispatching/Features/MakeEventPrivateHandler.cs
@@ -0,0 +1,30 @@
+﻿using EventAssociation.Core.Domain.Aggregates.Event;
+using EventAssociation.Core.Domain.Common;
+using EventAssociation.Core.Tools.OperationResult;
+
+namespace EventAssociation.Core.Application.CommandDispatching.Features;
+
+public class MakeEventPrivateHandler : ICommandHandler<MakeEventPrivateCommand>
+{
+    private readonly IEventRepository repository;
+    private readonly IUnitOfWork uow;
+
+    public async Task<Result<None>> HandleAsync(MakeEventPrivateCommand command)
+    {
+        var getEvent = await repository.GetAsync(command.Id);
+        if (!getEvent.IsSuccess)
+        {
+            return Result<None>.Err(getEvent.UnwrapErr().ToArray());
+        }
+
+        var changeEventTypeToPrivate = getEvent.Unwrap().ChangeEventTypeToPrivate();
+
+        if (changeEventTypeToPrivate.IsSuccess)
+        {
+            return changeEventTypeToPrivate;
+        }
+
+        await uow.SaveChangesAsync();
+        return Result<None>.Err(changeEventTypeToPrivate.UnwrapErr().ToArray());
+    }
+}
