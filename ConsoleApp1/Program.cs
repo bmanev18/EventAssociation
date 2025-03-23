516838cacbf073ffd4730a36bc1e4e6f2442e93f
Ameya <ameya.mahankal@gmail.com>
2025-03-23 19:54:36 +0100

    sanity check explicit operator works

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,39 +4,52 @@ using EventAssociation.Core.Domain.Aggregates.Event;
 using EventAssociation.Core.Domain.Aggregates.Event.Values;
 using EventAssociation.Core.Domain.Aggregates.Guests;
 using EventAssociation.Core.Domain.Aggregates.Guests.Values;
+using EventAssociation.Core.Domain.Aggregates.Invitation;
 using EventAssociation.Core.Domain.Aggregates.Locations;
 using EventAssociation.Core.Domain.Aggregates.Locations.Values;
 using EventAssociation.Core.Tools.OperationResult;
+//
+// var locationType = LocationType.Outside;
+// var locationName = LocationName.Create("Meadows").Unwrap();
+// var locationCapacity = LocationCapacity.Create(100).Unwrap();
+// var location = Location.CreateLocation(locationType, locationName, locationCapacity).Unwrap();
+//
+// var eventType = EventType.Public;
+// var eventStartDate = new EventTime(new DateTime(2026, 01, 01));
+// var eventEndDate = new EventTime(new DateTime(2026, 01, 01));
+// var eventResult = Event.CreateEvent(location, eventType, eventStartDate, eventEndDate);
+// var newEvent = eventResult.Unwrap();
+//
+// var eventTitle = EventTitle.CreateEventTitle("imaginal disk event");
+// var eventDesc = EventDescription.CreateEventDescription("imaginal disk event description");
+//
+//
+// newEvent.ChangeTitle(eventTitle.Unwrap());
+// newEvent.ChangeDescription(eventDesc.Unwrap());
+//
+//
+// var guestName = GuestName.Create("hello", "hello").Unwrap();
+// var guestEmail = GuestVIAEmail.Create("abv@via.dk").Unwrap();
+//
+// var validUrl = new Uri("https://example.com/profile.jpg");
+// var guestImage = GuestImageUrl.Create(validUrl).Unwrap();
+//
+// var guest = Guest.Create(guestName, guestEmail, guestImage, null).Unwrap();
+// newEvent.ChangeEventStatusToActive();
+//
+// var result = newEvent.RegisterGuestToEvent(guest);
+// Console.WriteLine(result.IsSuccess);
+// Using a valid GUID string
+string validGuid = "3F2504E0-4F89-41D3-9A0C-0305E82C3301";
 
-var locationType = LocationType.Outside;
-var locationName = LocationName.Create("Meadows").Unwrap();
-var locationCapacity = LocationCapacity.Create(100).Unwrap();
-var location = Location.CreateLocation(locationType, locationName, locationCapacity).Unwrap();
+// Creating an EventId with the GUID string
+EventId eventId = EventId.Create(validGuid).Unwrap();
 
-var eventType = EventType.Public;
-var eventStartDate = new EventTime(new DateTime(2026, 01, 01));
-var eventEndDate = new EventTime(new DateTime(2026, 01, 01));
-var eventResult = Event.CreateEvent(location, eventType, eventStartDate, eventEndDate);
-var newEvent = eventResult.Unwrap();
+// Assigning eventId to InvitationEventId (assuming implicit conversion is allowed)
+InvitationEventId invitationEventId = eventId;
 
-var eventTitle = EventTitle.CreateEventTitle("imaginal disk event");
-var eventDesc = EventDescription.CreateEventDescription("imaginal disk event description");
-
-
-newEvent.ChangeTitle(eventTitle.Unwrap());
-newEvent.ChangeDescription(eventDesc.Unwrap());
-
-
-var guestName = GuestName.Create("hello", "hello").Unwrap();
-var guestEmail = GuestVIAEmail.Create("abv@via.dk").Unwrap();
-
-var validUrl = new Uri("https://example.com/profile.jpg");
-var guestImage = GuestImageUrl.Create(validUrl).Unwrap();
-
-var guest = Guest.Create(guestName, guestEmail, guestImage, null).Unwrap();
-newEvent.ChangeEventStatusToActive();
-
-var result = newEvent.RegisterGuestToEvent(guest);
-Console.WriteLine(result.IsSuccess);
+// Printing out the values
+Console.WriteLine(eventId.ToString());
+Console.WriteLine(invitationEventId.ToString());
 
 
