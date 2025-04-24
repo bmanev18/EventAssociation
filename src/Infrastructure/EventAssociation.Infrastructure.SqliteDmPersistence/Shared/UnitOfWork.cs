9aed337f0c2f129a5c21f1fc8771a5b50eb68855
Radoslav Kiryazov <100125411+radoslavKiryazov@users.noreply.github.com>
2025-04-24 15:49:20 +0200

    Created UnitOfWork

---
+++ src/Infrastructure/EventAssociation.Infrastructure.SqliteDmPersistence/Shared/UnitOfWork.cs
@@ -0,0 +1,28 @@
+using EventAssociation.Core.Domain.Common;
+using EventAssociation.Core.Tools.OperationResult;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventAssociation.Infrastructure.SqliteDmPersistence.Shared;
+
+public class EfcUnitOfWork : IUnitOfWork
+{
+    private readonly DbContext _context;
+ 
+    public EfcUnitOfWork(DbContext context)
+    {
+        _context = context;
+    }
+ 
+    public async Task<Result<None>> SaveChangesAsync()
+    {
+        try
+        {
+            await _context.SaveChangesAsync();
+            return Result<None>.Ok(None.Value);
+        }
+        catch (Exception ex)
+        {
+            return Result<None>.Err(new Error("0", $"Error during SaveChanges: {ex.Message}"));
+        }
+    }
+}
