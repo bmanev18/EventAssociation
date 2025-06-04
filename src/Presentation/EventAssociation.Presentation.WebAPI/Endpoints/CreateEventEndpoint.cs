dcd9f099ae03dc0aef36a30abd5cec4b3401473e
Ameya Mahankal <ameya.mahankal@gmail.com>
2025-06-04 13:58:03 +0200

    reprebase,endpoint created

---
+++ src/Presentation/EventAssociation.Presentation.WebAPI/Endpoints/CreateEventEndpoint.cs
@@ -0,0 +1,22 @@
+﻿using EventAssociation.Core.Application.CommandDispatching.Commands;
+using Microsoft.AspNetCore.Mvc;
+using Endpoint = WebAPI.REPRBase.Endpoint;
+
+namespace WebAPI.Endpoints;
+
+public class CreateEventEndpoint : Endpoint.WithRequest<CreateEventEndpointRequest>.WithoutResponse
+{
+
+
+    public override Task<ActionResult> HandleAsync(CreateEventEndpointRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public record CreateEventEndpointRequest()
+{
+    
+}
+
+
