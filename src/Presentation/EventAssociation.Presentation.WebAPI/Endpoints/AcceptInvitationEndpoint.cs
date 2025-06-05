1087d5c5c4a4642e01ed759c75ad2387ae4ccfc5
Radoslav Kiryazov <100125411+radoslavKiryazov@users.noreply.github.com>
2025-06-05 11:29:41 +0200

    new endpoints

---
+++ src/Presentation/EventAssociation.Presentation.WebAPI/Endpoints/AcceptInvitationEndpoint.cs
@@ -0,0 +1,33 @@
+﻿using EventAssociation.Core.Application.CommandDispatching;
+using EventAssociation.Core.Application.CommandDispatching.Commands;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.ActionResultHelper;
+using Endpoint = WebAPI.REPRBase.Endpoint;
+
+namespace WebAPI.Endpoints;
+
+public class AcceptInvitationEndpoint(ICommandDispatcher dispatcher)
+    : Endpoint.WithRequest<AcceptInvitationEndpointRequest>.WithoutResponse
+{
+    [HttpPost("invitations/{InvitationId}/accept")]
+    public override async Task<ActionResult> HandleAsync(AcceptInvitationEndpointRequest request)
+    {
+        var commandResult = AcceptInvitationCommand.Create(request.InvitationId);
+
+        if (!commandResult.IsSuccess)
+        {
+            return ActionResultConverter.FromErrors(commandResult.UnwrapErr());
+        }
+
+        var handlerResult = await dispatcher.DispatchAsync(commandResult.Unwrap());
+
+        if (!handlerResult.IsSuccess)
+        {
+            return ActionResultConverter.FromErrors(handlerResult.UnwrapErr());
+        }
+
+        return Ok();
+    }
+    
+}
+public record AcceptInvitationEndpointRequest([FromRoute] string InvitationId);
---
+++ src/Presentation/EventAssociation.Presentation.WebAPI/Endpoints/UpdateEventTimesEndpoint.cs
@@ -0,0 +1,47 @@
+﻿using EventAssociation.Core.Application.CommandDispatching;
+using EventAssociation.Core.Application.CommandDispatching.Commands;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.ActionResultHelper;
+using Endpoint = WebAPI.REPRBase.Endpoint;
+
+namespace WebAPI.Endpoints;
+
+public class UpdateEventTimesEndpoint(ICommandDispatcher dispatcher)
+    : Endpoint.WithRequest<UpdateEventTimesEndpointRequest>.WithoutResponse
+{
+    [HttpPatch("events/update-times")]
+    public override async Task<ActionResult> HandleAsync(UpdateEventTimesEndpointRequest request)
+    {
+        var commandResult = UpdateEventTimesCommand.Create(
+            request.RequestBody.EventId,
+            request.RequestBody.StartDateTime,
+            request.RequestBody.EndDateTime
+        );
+
+        if (!commandResult.IsSuccess)
+        {
+            return ActionResultConverter.FromErrors(commandResult.UnwrapErr());
+        }
+
+        var handlerResult = await dispatcher.DispatchAsync(commandResult.Unwrap());
+
+        if (!handlerResult.IsSuccess)
+        {
+            return ActionResultConverter.FromErrors(handlerResult.UnwrapErr());
+        }
+
+        return Ok();
+    }
+}
+
+
+public record UpdateEventTimesEndpointRequest(
+    [FromBody] UpdateEventTimesEndpointRequest.Body RequestBody
+)
+{
+    public record Body(
+        string EventId,
+        string StartDateTime,
+        string EndDateTime
+    );
+}
